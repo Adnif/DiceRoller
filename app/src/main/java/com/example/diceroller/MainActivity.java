38de21753182c8270cc0a1618b1ecef86231eebb
Adnif <ahmadbani9@gmail.com>
2022-10-05 23:57:33 +0700

    DiceRoller Commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.diceroller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rollDice();
+            }
+        });
+    }
+
+    private void rollDice(){
+        Random rng = new Random();
+        ImageView diceImage;
+        diceImage = findViewById(R.id.imageView);
+        int randomNumber = rng.nextInt(6) + 1;
+
+        switch(randomNumber){
+            case 1:
+                diceImage.setImageResource(R.drawable.dice_1);
+                break;
+            case 2:
+                diceImage.setImageResource(R.drawable.dice_2);
+                break;
+            case 3:
+                diceImage.setImageResource(R.drawable.dice_3);
+                break;
+            case 4:
+                diceImage.setImageResource(R.drawable.dice_4);
+                break;
+            case 5:
+                diceImage.setImageResource(R.drawable.dice_5);
+                break;
+            case 6:
+                diceImage.setImageResource(R.drawable.dice_6);
+                break;
+        }
+    }
+
+
+}
